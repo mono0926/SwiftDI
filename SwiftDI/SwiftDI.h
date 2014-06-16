ef895fb745916561c386f991d4bd1710cab05f9a
Masayuki Ono <ono@ripplex.com>
2014-06-16 09:00:00 +0900

    setup initial projects.

---
+++ SwiftDI/SwiftDI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDI.h
+//  SwiftDI
+//
+//  Created by mono on 6/16/14.
+//  Copyright (c) 2014 mono. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for SwiftDI.
+FOUNDATION_EXPORT double SwiftDIVersionNumber;
+
+//! Project version string for SwiftDI.
+FOUNDATION_EXPORT const unsigned char SwiftDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDI/PublicHeader.h>
+
+
